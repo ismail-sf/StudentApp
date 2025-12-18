0f5bb298a578e75ed9fe940370f6ed7a131f2d18
ismail-sf <ISafha@bab-distribution.de>
2025-12-18 09:34:58 +0100

    Add project files.

---
+++ Crud-app-mvc-1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Crud_app_mvc_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crud_app_mvc_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Crud-app-mvc-1/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Crud_app_mvc_1.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud_app_mvc_1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
